3e4d25369d45aad79dbbdeffc393773ad63799ae
william <william_134@163.com>
2019-07-29 15:52:21 +0800

    add all files

---
+++ cn/william/demo/DemoTest.java
@@ -0,0 +1,47 @@
+package cn.william.demo; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After;
+import org.junit.*;
+
+/** 
+* Demo Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>���� 26, 2019</pre> 
+* @version 1.0 
+*/ 
+public class DemoTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: add(int i, int j) 
+* 
+*/ 
+@Test
+public void testAdd() throws Exception { 
+//TODO: Test goes here...
+    
+} 
+
+/** 
+* 
+* Method: sub(int i, int j) 
+* 
+*/ 
+@Test
+public void testSub() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
---
+++ util/DBUtilTest.java
@@ -0,0 +1,45 @@
+package util; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* DBUtil Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>07/18/2019</pre> 
+* @version 1.0 
+*/ 
+public class DBUtilTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getConnection() 
+* 
+*/ 
+@Test
+public void testGetConnection() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: close(Connection connection, PreparedStatement pas, ResultSet rs) 
+* 
+*/ 
+@Test
+public void testClose() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
